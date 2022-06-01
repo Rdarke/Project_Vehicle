746a2175c298534f42d1bdd5a4c6437bc411b9a5
Rdarke <rickydarke1@gmail.com>
2022-06-01 10:57:04 -0700

    Add testing button for api request

--- app/src/components/app/index.jsx
+++ app/src/components/app/index.jsx
@@ -1,25 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [ state, setState ] = useState({
+    message: 'Click the button to load API data!'
+  });
+
+  const fetchData = () => {
+    axios.get('/api/data')
+    .then((res) => {
+      const message = res.data.message;
+      console.log(res.data)
+      console.log(res.data.message)
+      setState(prev => ({ ...prev, message}))
+    })
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>{ state.message }</h1>
+        <button onClick={fetchData} >
+          Fetch Data
+        </button> 
     </div>
   );
-}
+};
 
 export default App;
