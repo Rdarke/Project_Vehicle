a32e2cc541950dae421fa2bd64b74b1951664710
Rdarke <rickydarke1@gmail.com>
2022-06-01 12:36:13 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ App.use(BodyParser.urlencoded({ extended: false }));
 App.use(BodyParser.json());
 App.use(Express.static('public'));
 
+
+
 // Sample GET route
 App.get('/api/data', (req, res) => res.json({
   message: "Seems to work!",
