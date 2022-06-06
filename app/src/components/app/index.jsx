52765d383d3f588c55f68b22a11e75f17f4ede5f
Rdarke <rickydarke1@gmail.com>
2022-06-06 16:16:05 -0700

    convert app.css to .scss fileformat

--- app/src/components/app/index.jsx
+++ app/src/components/app/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import axios from 'axios';
-import './App.css';
+import './App.scss';
 import Button from "@mui/material/Button";
 import Navbar from '../navbar';
 
