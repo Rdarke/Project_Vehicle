b4129ffc9cc5cb68acc0cb0c7e6c9595b4a1b9fe
Rdarke <rickydarke1@gmail.com>
2022-06-06 16:21:09 -0700

    Update storybook button states

---
+++ app/src/stories/index.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import "../index.scss";
+
+storiesOf("Button", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Base", () => <Button>Base</Button>)
+  .add("Confirm", () => <Button confirm>Confirm</Button>)
+  .add("Danger", () => <Button danger>Cancel</Button>)
+  .add("Clickable", () => (
+    <Button onClick={action("button-clicked")}>Clickable</Button>
+  ))
+  .add("Disabled", () => (
+    <Button disabled onClick={action("button-clicked")}>
+      Disabled
+    </Button>
+  ));
