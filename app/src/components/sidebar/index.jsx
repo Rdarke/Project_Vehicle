9cb407815f337a784c8f0e60607efcd8117bb14f
Rdarke <rickydarke1@gmail.com>
2022-07-13 13:50:48 -0700

    Add My favorites menuitem to sidebar

--- app/src/components/sidebar/index.jsx
+++ app/src/components/sidebar/index.jsx
@@ -18,16 +18,25 @@ function Sidebar(props) {
           name={"Home"}
           selected={selected}
           setMenuItem={updateSelected}
+          link={"home"}
         ></MenuItem>
         <MenuItem
           name={"My Garage"}
           selected={selected}
           setMenuItem={updateSelected}
+          link={"my-garage"}
+        ></MenuItem>
+        <MenuItem
+          name={"My Favorites"}
+          selected={selected}
+          setMenuItem={updateSelected}
+          link={"my-favorites"}
         ></MenuItem>
         <MenuItem
           name={"Search"}
           selected={selected}
           setMenuItem={updateSelected}
+          link={"search"}
         ></MenuItem>
       </nav>
     </section>
