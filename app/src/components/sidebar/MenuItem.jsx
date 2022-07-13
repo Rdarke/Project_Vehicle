7ff1e5b3031b7c698e27f5913a475dede9aca435
Rdarke <rickydarke1@gmail.com>
2022-07-12 20:18:11 -0700

    Pass dynamic style rendering via props to sidebar component

--- app/src/components/sidebar/MenuItem.jsx
+++ app/src/components/sidebar/MenuItem.jsx
@@ -1,15 +1,24 @@
+import React, { useState } from "react";
 import classNames from 'classnames';
 import "./MenuItem.scss";
 
 function MenuItem (props) {
+  const [style, setStyle] = useState(false)
   const { setMenuItem, name, selected } = props;
-
   const menuItemClass = classNames("menu-list__item", {
-    "menu-list__item--selected": selected,
+    "menu-list__item--selected": style,
   });
 
+  // if (selected === name) {
+  //   setStyle(true);
+  // };
+
+  const saveMenu = () => {
+    setMenuItem(name)
+  };
+
   return (
-    <li className={menuItemClass} onClick={() => setMenuItem(name)}>
+    <li className={menuItemClass} onClick={() => saveMenu()}>
     <h2>{name}</h2>
     </li>
   );
