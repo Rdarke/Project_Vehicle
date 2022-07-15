e94632cd5ab53d986026395d1c832d1da34975af
Rdarke <rickydarke1@gmail.com>
2022-07-14 19:37:52 -0700

    Fix bug within Form event handling

--- app/src/components/form/Form.jsx
+++ app/src/components/form/Form.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 
 function Form(props) {
-  const [myOption, setMyOption] = useState("Make");
+  const [myOption, setMyOption] = useState("");
   const { options, saveSelection } = props;
 
   const handleChange = (event) => {
+    event.preventDefault();
     setMyOption(event.target.value);
+    share();
   };
 
   const share = () => {
@@ -14,7 +16,7 @@ function Form(props) {
 
   return (
     <form>
-      <select value={myOption} onChange={handleChange, share}>
+      <select value={myOption} onChange={handleChange}>
         {options.map((element) => (
           <option value={element}>{element}</option>
         ))}
--- app/src/components/search/Search.jsx
+++ app/src/components/search/Search.jsx
@@ -2,12 +2,30 @@ import React, { useState } from 'react';
 import Form from '../form/Form';
 
 function Search(props) {
+  const [ state, setState] = useState({
+    make: "",
+    model: "",
+    year: ""
+  })
+
+  const saveSelection = (data) => {
+    setState({...state, data})
+  }
 
   return (
-    <div>
+    <div className="search">
       <p>Search Section</p>
       <Form
       options={["Ford", "Fiat", "Chevy", "BMW", "Mercedes", "GMC", "Jeep"]}
+      saveSelection={saveSelection}
+      >
+      </Form>
+      <Form
+      options={[""]}
+      >
+      </Form>
+      <Form
+      options={[""]}
       >
       </Form>
     </div>
