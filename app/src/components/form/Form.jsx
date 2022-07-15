db9708fe799f90c11da169eec7eb68c0f9af9891
Rdarke <rickydarke1@gmail.com>
2022-07-14 18:29:17 -0700

    Create Form.jsx initial component

---
+++ app/src/components/form/Form.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+function Form(props) {
+  const [myOption, setMyOption] = useState("Alpha");
+
+  const handleChange = (event) => {
+    setMyCar(event.target.value)
+  }
+
+  return (
+    <form>
+      <select value={myCar} onChange={handleChange}>
+        <option value="Ford">Ford</option>
+        <option value="Volvo">Volvo</option>
+        <option value="Fiat">Fiat</option>
+      </select>
+    </form>
+  )
+}
+
+export default Form;
