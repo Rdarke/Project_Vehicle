7b12c46b6c31d4458387f94029ed86dd709f94c8
Rdarke <rickydarke1@gmail.com>
2022-07-19 20:21:12 -0700

    Create VehicleView.scss styling and import

--- app/src/components/vehicleCard/index.jsx
+++ app/src/components/vehicleCard/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-import "./vehicleCard.scss";
+import "./VehicleCard.scss";
 import Button from '../button';
 
 
--- app/src/components/vehicleView/VehicleView.jsx
+++ app/src/components/vehicleView/VehicleView.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { useParams } from 'react-router-dom';
+import './VehicleView.scss';
 
 function VehicleView() {
   const params = useParams(); // Provides URL param.id
 console.log("This is the params", params)
   return (
-    <div>Hello single view - {`${params.id}`}</div>
+    <div className={'vehicleView__wrapper'}>
+    <h1 className={'vehicleView__title'}> Title Goes Here!</h1>
+    <div className={'vehicleView__hero'}>
+      <p>Hero Image Here</p>
+    </div>
+    <div className={'vehicleView__overview'}>
+      <p>Overview!</p>
+    </div>
+
+    </div>
   )
 };
 
