addd3254b38a288aae68069eaa00dbb719fceee5
Rdarke <rickydarke1@gmail.com>
2022-07-19 18:46:58 -0700

    Refactor VehicleCard display

--- app/src/components/app/index.jsx
+++ app/src/components/app/index.jsx
@@ -1,84 +1,73 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
-import axios from 'axios';
-import './App.scss';
-import Sidebar from '../sidebar';
-import VehicleCard from '../vehicleCard';
-import Search from '../search/Search';
-import mockVehicleData from '../../mockData/vehicleCardMock.json';
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Switch,
+} from "react-router-dom";
+import axios from "axios";
+import "./App.scss";
+import Sidebar from "../sidebar";
+import VehicleCard from "../vehicleCard";
+import Search from "../search/Search";
+import mockVehicleData from "../../mockData/vehicleCardMock.json";
 // import Navbar from '../navbar';
 // import Button from '../button'
 
 function App() {
-  const [ state, setState ] = useState({
-    message: 'Click the button to load API data!'
+  const [state, setState] = useState({
+    message: "Click the button to load API data!",
+    cards: mockVehicleData,
   });
 
-  const fetchData = () => {
-    axios.get('/api/data')
-    .then((res) => {
-      const message = res.data.message;
-      console.log(res.data)
-      console.log(res.data.message)
-      setState(prev => ({ ...prev, message}))
-    })
-  };
+  // Example fetch data for api connection ----
+  // const fetchData = () => {
+  //   axios.get('/api/data')
+  //   .then((res) => {
+  //     const message = res.data.message;
+  //     console.log(res.data)
+  //     console.log(res.data.message)
+  //     setState(prev => ({ ...prev, message}))
+  //   })
+  // };
+
+  const displayVehicleCards = state.cards.map((vehicle) => {
+    return (
+      <VehicleCard
+        key={vehicle.id}
+        heroImage={vehicle.heroImage}
+        title={vehicle.title}
+        make={vehicle.make}
+        description={vehicle.description}
+      ></VehicleCard>
+    );
+  });
 
   return (
     <main className="layout">
-    <Router>
+      <Router>
+        <Sidebar></Sidebar>
+        <Routes>
+          <Route path="/my-favorites" element={<p>I like these</p>}></Route>
 
-    <Sidebar></Sidebar>
-    <Routes>
+          <Route path="/my-garage" element={<p>This is the garage!</p>}></Route>
 
-    <Route path="/my-favorites" element={<p>I like these</p>}>
-    </Route>
+          <Route path="/search" element={<p>This is the search!</p>}></Route>
 
-    <Route path="/my-garage" element={<p>This is the garage!</p>}>
-    </Route>
-
-    <Route path="/search" element={<p>This is the search!</p>}>
-    </Route>
-
-    <Route path="/home" element={<section className="content">
-    <h3>This is the content section!</h3>
-
-    <Search></Search>  
-
-    <VehicleCard
-      heroImage={"https:www.motortrend.com/uploads/sites/5/2019/10/SEMA-2019-Hellwig-Ford-Ranger.jpg"}
-      title={"MotorTrend Masterpiece"}
-      make={"Ford - Ranger - 2020"}
-      description={"Something clever about this build. I like to go fast like Ricky Bobby and some more information to make this description long enough for testing screen widths...etc"}
-      ></VehicleCard>
-      <VehicleCard
-      heroImage={"https:www.motortrend.com/uploads/sites/5/2019/10/SEMA-2019-Hellwig-Ford-Ranger.jpg"}
-      title={"MotorTrend Masterpiece"}
-      make={"Ford - Ranger - 2020"}
-      description={"Something clever about this build. I like to go fast like Ricky Bobby and some more information to make this description long enough for testing screen widths...etc"}
-      ></VehicleCard>
-      <VehicleCard
-      heroImage={"https:www.motortrend.com/uploads/sites/5/2019/10/SEMA-2019-Hellwig-Ford-Ranger.jpg"}
-      title={"MotorTrend Masterpiece"}
-      make={"Ford - Ranger - 2020"}
-      description={"Something clever about this build. I like to go fast like Ricky Bobby and some more information to make this description long enough for testing screen widths...etc"}
-      ></VehicleCard>
-      <VehicleCard
-      heroImage={""}
-      title={"MotorTrend Masterpiece"}
-      make={"Ford - Ranger - 2020"}
-      description={"Something clever about this build. I like to go fast like Ricky Bobby and some more information to make this description long enough for testing screen widths...etc"}
-      ></VehicleCard>
-
-      
-    </section>}>
-    </Route>
-
-    </Routes>
-
-    </Router>
+          <Route
+            path="/home"
+            element={
+              <section className="content">
+                <h3>This is the content section!</h3>
+                <Search></Search>
+                {displayVehicleCards}
+              </section>
+            }
+          ></Route>
+        </Routes>
+      </Router>
     </main>
   );
-};
+}
 
 export default App;
