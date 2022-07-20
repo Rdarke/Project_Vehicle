cf3ee032f747857d5efc47a052fdd17bed956752
Rdarke <rickydarke1@gmail.com>
2022-07-19 17:41:37 -0700

    Update className to remove rendering error

--- app/src/components/form/Form.jsx
+++ app/src/components/form/Form.jsx
@@ -12,7 +12,7 @@ function Form(props) {
 
   return (
     <form className="form">
-      <select classname="form__selection" value={myOption} onChange={handleChange}>
+      <select className="form__selection" value={myOption} onChange={handleChange}>
         {options.map((element) => (
           <option className="form__option" value={element} key={element}>{element}</option>
         ))}
