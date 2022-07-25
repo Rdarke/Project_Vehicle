a49db6510b237a6b602240265d6e4a47d19ec61f
Rdarke <rickydarke1@gmail.com>
2022-07-25 12:06:40 -0700

    Update lint error

--- app/src/components/vehicleView/VehicleView.jsx
+++ app/src/components/vehicleView/VehicleView.jsx
@@ -13,18 +13,22 @@ function VehicleView() {
         <h2 className={"vehicle__title"}>
           Title Goes Here! & Here & Here & Here & Here &........
         </h2>
-          <p>Something clever about this build. I like to go fast like Ricky Bobby and some more information to make this description long enough for testing screen widths...etc</p>
+        <p>
+          Something clever about this build. I like to go fast like Ricky Bobby
+          and some more information to make this description long enough for
+          testing screen widths...etc
+        </p>
         <ul classname={"vehicle__details"}>
-        <li>Make: Ford</li>
-        <li>Model: Ranger</li>
-        <li>Year: 2020</li>
-        <li>Years Owned: 2</li>
+          <li>Make: Ford</li>
+          <li>Model: Ranger</li>
+          <li>Year: 2020</li>
+          <li>Years Owned: 2</li>
         </ul>
       </div>
       <div className={"gallery__wrapper"}>
-      <Gallery></Gallery>
+        <Gallery></Gallery>
       </div>
-      </div>
+    </div>
   );
 }
 
